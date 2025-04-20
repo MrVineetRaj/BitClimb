21e73edea0114b22a1036309df2c6124930d7008
MrVineetRaj <vineetrajrj26@gmail.com>
2025-04-20 13:50:59 +0530

    Update : Now Sentry setup is done  and will work only if NODE_ENV is production

--- backend/sentry.js
+++ backend/sentry.js
@@ -3,6 +3,9 @@ import "dotenv/config";
 
 import * as Sentry from "@sentry/node";
 
+console.log(process.env.SENTRY_DNS);
 Sentry.init({
   dsn: process.env.SENTRY_DNS,
+  environment: process.env.NODE_ENV || "development",
+  tracesSampleRate: 1.0, // Adjust this value in production
 });
--- backend/src/app.js
+++ backend/src/app.js
@@ -7,19 +7,22 @@ dotenv.config();
 
 const app = express();
 
-// The error handler must be registered before any other error middleware and after all controllers
-Sentry.setupExpressErrorHandler(app);
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
 
+let isProduction = process.env.NODE_ENV === "production";
+
+// The error handler must be registered before any other error middleware and after all controllers
+if (isProduction) {
+  Sentry.setupExpressErrorHandler(app);
+}
 // Optional fallthrough error handler
 app.use(function onError(err, req, res, next) {
   // The error id is attached to `res.sentry` to be returned
   // and optionally displayed to the user for support.
   res.statusCode = 500;
   res.end(res.sentry + "\n");
-});
-
-app.get("/debug-sentry", function mainHandler(req, res) {
-  throw new Error("My first Sentry error!");
 });
 
 export default app;
