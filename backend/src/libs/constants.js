dee85f7cd045b1556bf677e25cf12554f34e0a98
MrVineetRaj <vineetrajrj26@gmail.com>
2025-05-31 20:36:23 +0530

    Fix : updated cookie options made samesite="none" (  for testing my code execution on internet)

--- backend/src/libs/constants.js
+++ backend/src/libs/constants.js
@@ -1,7 +1,7 @@
 export const CookieOptions = {
   maxAge: 7 * 24 * 60 * 60 * 1000,
   httpOnly: process.env.NODE_ENV !== "development",
-  sameSite: "strict",
+  sameSite: "none",
   secure: process.env.NODE_ENV !== "development",
 };
 
